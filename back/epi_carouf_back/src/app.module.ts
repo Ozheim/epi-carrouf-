14ee6430cdb988362aa886f3690ba877ec75b7ec
Kylian Labrador <kylian.labrador@epitech.eu>
2024-11-21 15:39:51 +0100

    [ADD] MongoDB init and User Route and User Schema done.

--- back/epi_carouf_back/src/app.module.ts
+++ back/epi_carouf_back/src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/epi_carouf_db', {
+      connectTimeoutMS: 10000,
+      serverSelectionTimeoutMS: 5000,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
