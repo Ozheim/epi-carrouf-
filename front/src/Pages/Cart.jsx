17649a9ed03f0581ba3b5d2bc7ca1bae42b845e9
Ozheim <heikel.zouaoui@epitech.eu>
2024-11-22 17:24:27 +0100

    Store implemented

---
+++ front/src/Pages/Cart.jsx
@@ -0,0 +1,17 @@
+const Cart  = () => {
+
+
+
+
+
+
+    return(
+        <div>
+            <h1>Vos articles : </h1>
+            {}
+
+        </div>
+    )
+}
+
+export default Cart;
--- front/src/appRouter.jsx
+++ front/src/appRouter.jsx
@@ -4,6 +4,7 @@ import Login from './Pages/Login.jsx';
 import SignUp from './Pages/SignUp.jsx';
 import PrivateRoutes from './Utils/PrivateRoutes.jsx';
 import Store from "../src/Pages/Store.jsx";
+import Cart from "./Pages/Cart.jsx";
 
 
 const AppRouter =() =>{
@@ -13,7 +14,8 @@ const AppRouter =() =>{
             <Route path="/Home" element={<Home />} />
             <Route path="/Login" element={<Login/>}/>
             <Route path="/SignUp" element={<SignUp/>}/>
-            <Route path="/Store" element={<Store />}/>
+            <Route path="/Store" element={<Store />}/>  
+            <Route path="/Panier" element={<Cart/>} />
                    
                 {/* <Route
                 path="/Store"
