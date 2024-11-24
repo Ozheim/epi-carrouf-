c05af6f88dcf0a903439cc8954b904c3a7e04a89
Ozheim <heikel.zouaoui@epitech.eu>
2024-11-24 15:44:56 +0100

    style update

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./appRouter.jsx";
+import "./Styles/main.scss";
 
 const App = () => {
   return (
--- front/src/Pages/Login.jsx
+++ front/src/Pages/Login.jsx
@@ -1,4 +1,4 @@
-import "../Styles/Pages/Login.scss";
+
 import { useState } from "react";
 import { useNavigate } from "react-router";
 
